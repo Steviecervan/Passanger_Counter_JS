73ebd0673f4d34df6fc121b448f87aa48df55a2b
Stevie-Cervan <stevie.cervan06@gmail.com>
2021-12-31 02:09:19 -0700

    Learned mathematical expressions

---
+++ index.js
@@ -0,0 +1,9 @@
+// document.getElementById("count").innerText = 5
+
+let bonusPoints = 50;
+bonusPoints = bonusPoints + 50;
+console.log(bonusPoints);
+bonusPoints = bonusPoints - 75;
+console.log(bonusPoints);
+bonusPoints = bonusPoints + 45;
+console.log(bonusPoints);
